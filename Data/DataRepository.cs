502581de164a35b928a881cae842106026fad02f
Alice Triebull <dotnewzero@gmail.com>
2019-07-08 10:09:46 +0200

    Further work on HostLists

--- Data/DataRepository.cs
+++ Data/DataRepository.cs
@@ -110,6 +110,24 @@ namespace ObservingThingy.Data
                     .ToListAsync();
         }
 
+        internal async Task<HostList> Get(int id)
+        {
+            using (var context = _factory())
+                return await context.HostLists
+                    .Include(x => x.HostListToHosts)
+                    .ThenInclude(x => x.Host)
+                    .SingleAsync(x => x.Id == id);
+        }
+
+        internal async Task<List<Host>> GetHosts(int id)
+        {
+            using (var context = _factory())
+                return (await this.Get(id))
+                    .HostListToHosts
+                    .Select(x => x.Host)
+                    .ToList();
+        }
+
         internal async Task Create(HostList hostlist)
         {
             using (var context = _factory())
@@ -133,6 +151,31 @@ namespace ObservingThingy.Data
             using (var context = _factory())
             {
                 context.HostLists.Remove(hostlist);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        internal async Task AddHostToHostList(Host host, HostList hostlist)
+        {
+            using (var context = _factory())
+            {
+                var link = new HostListToHost
+                {
+                    Host = host,
+                    HostList = hostlist
+                };
+                hostlist.HostListToHosts.Add(link);
+                context.HostLists.Update(hostlist);
+                await context.SaveChangesAsync();
+            }
+        }
+        internal async Task RemoveHostFromHostList(Host host, HostList hostlist)
+        {
+            using (var context = _factory())
+            {
+                var link = hostlist.HostListToHosts.Single(x => x.HostId == host.Id && x.HostListId == hostlist.Id);
+                context.Set<HostListToHost>()
+                    .Remove(link);
                 await context.SaveChangesAsync();
             }
         }
