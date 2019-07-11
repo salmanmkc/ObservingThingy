8c217685bf508c5056463d458131a559685cb9cb
Alice Triebull <dotnewzero@gmail.com>
2019-07-11 09:23:01 +0200

    Putting host states in memory

--- Data/Host.cs
+++ Data/Host.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace ObservingThingy.Data
@@ -6,7 +5,6 @@ namespace ObservingThingy.Data
     public class Host : DataModel
     {
         public string Hostname { get; set; }
-        public List<HostState> States { get; set; }
 
         public List<Service> Services { get; set; }
 
@@ -14,29 +12,5 @@ namespace ObservingThingy.Data
         public List<TagToHost> TagToHosts { get; set; }
 
         public string Comment { get; set; }
-    }
-
-    public class HostState
-    {
-        public int Id { get; set; }
-
-        public Host Host { get; set; }
-        public int HostId { get; set; }
-
-        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.Now;
-
-        public int Delay { get; set; } = 0;
-
-        public StatusEnum Status { get; set; } = StatusEnum.Unchecked;
-
-        public enum StatusEnum : int
-        {
-            Unchecked = 0,
-            Online,
-            Warning,
-            Critical,
-            Offline,
-            Error
-        }
     }
 }
